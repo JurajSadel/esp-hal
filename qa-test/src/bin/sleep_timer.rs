d7337787a3e49b289b808712543d808fb8c1954f
Juraj Sadel <juraj.sadel@espressif.com>
2025-10-14 13:44:25 +0200

    wip10 -revert

--- qa-test/src/bin/sleep_timer.rs
+++ qa-test/src/bin/sleep_timer.rs
@@ -18,6 +18,11 @@ use esp_println::println;
 
 esp_bootloader_esp_idf::esp_app_desc!();
 
+static mut COUNTER: u32 = 42;
+
+// This would go in .bss (zero initializer)
+static mut BUFFER: [u8; 1024] = [0; 1024];
+
 #[main]
 fn main() -> ! {
     let peripherals = esp_hal::init(esp_hal::Config::default());
